ca38740a4981987510c83b33a2dc0667c9ab478d
Vann Liu <vann@cpcp.tw>
2024-04-16 11:29:56 +0800

    feat: LinkWithLang remove lang required

--- src/components/custom/LinkWithLang.tsx
+++ src/components/custom/LinkWithLang.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import { i18n } from '~~/i18n.config'
+import { useParams } from "next/navigation"
 import LinkWithLang from 'vanns-common-modules/dist/components/next/LinkWithLang'
 
 interface TypeProps {
@@ -9,11 +12,15 @@ interface TypeProps {
 }
 
 export default function LinkWithLangWrapper({href, lang, children, ...props}:TypeProps, ref:React.ReactNode){
+  const params = useParams()
+  const { lang:currentLang } = params
+  const targetLang = lang || currentLang
+
   if( !href ){
     return <span {...props}>{children}</span>
   }
 
-  const isDefaultLang = lang === i18n.defaultLocale.shortCode
+  const isDefaultLang = targetLang === i18n.defaultLocale.shortCode
   const path = isDefaultLang ?href :`/${lang}${href}`
 
   return <LinkWithLang {...props} href={path} lang={lang} defaultLang={i18n.defaultLocale.shortCode}>{children}</LinkWithLang>
